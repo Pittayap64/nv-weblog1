5b9b400f01f47bb71a13914d5a5e6d77e64c3e5e
Pittayap64 <141794856+Pittayap64@users.noreply.github.com>
2023-10-18 02:19:51 +0700

    folder perpared

---
+++ client/src/components/User/Index.vue
@@ -0,0 +1,35 @@
+  <template>
+ <div>
+<h2>Get all users</h2>
+<h4>จํานวนผู้ใช้งาน {{users.length}}</h4>
+<div v-for="user in users" v-bind:key="user.id">
+ <p>id: {{ user.id }}</p>
+ <p>ชืEอ-นามสกุล: {{ user.name }} - {{ user.lastname }}</p>
+ <p>email: {{ user.email }}</p>
+ <p>password: {{ user.password }}</p>
+ <p><button v-on:click="navigateTo('/user/'+user.id)">ดูข้อมูลผู้ใช้</button></p>
+ <hr>
+</div>
+</div>
+</template>
+   <script>
+   import UserService from '@/services/UserService'
+   export default {
+    data () {
+    return {
+    users: []
+    }
+    },
+    async created () {
+    this.users = (await UserService.index()).data
+    },
+    methods: {
+        navigateTo (route) {
+            this.$router.push(route)
+        },
+    }
+   }
+   </script>
+   <style scoped>
+   </style>
+   
--- server/src/controllers/UserController.js
+++ server/src/controllers/UserController.js
@@ -1,25 +1,79 @@
+const {User} = require('../models')
+
 module.exports = {
-    // get all user
-    index (req, res) {
-    res.send('เรียกขอ้ มูลผใู้ชง้านทDงัหมด')
-    },
-    // create user
-    create (req, res) {
-    res.send('ทําการสร้างผู้ใช้งาน: ' + JSON.stringify(req.body))
-    },
-    // edit user, suspend, active
-    put (req, res) {
-    res.send('ทาํการแกไ้ขผใุ้ชง้าน: ' + req.params.userId + ' : ' + 
-   JSON.stringify(req.body))
-    },
-    // delete user
-    remove (req, res) {
-        res.send('ทําการลบผุ้ใช้งาน: ' + req.params.userId + ' : ' + 
-       JSON.stringify(req.body))
-        },
-        // get user by id
-        show (req, res) {
-        res.send('ดูข้อมูลผู้ใช้งาน: ' + req.params.userId)
+
+ // get all user
+ async index (req, res) {
+    //res.send('ดูข้อมูลผู้ใช้ทั้งหมด')
+    try {
+        const users = await User.findAll()
+        res.send(users)
+        } catch (error){
+        res.status(500).send({
+            error: 'The users information was incorrect'
+            })
         }
-       }
-       
+    },
+// create user
+async create (req, res) { 
+    try {
+        const user = await User.create(req.body)
+        res.send(user.toJSON())
+        } catch (err) {
+        res.status(500).send({
+            error: 'Create user incorrect'
+            })
+        }
+    },
+
+ // edit user, suspend, active
+ async put (req, res) {
+    try {
+        await User.update(req.body, {
+        where: {
+            id: req.params.userId
+            }
+        })
+        res.send(req.body)
+        } catch (err) {
+        res.status(500).send({
+          error: 'Update user incorrect' 
+        })  
+    }    
+},
+ // delete user
+async delete (req, res) {
+    try {
+        const user = await User.findOne({
+            where: {
+                id: req.params.userId
+            }
+        })
+
+        if(!user){
+            return res.status(403).send({
+                error: 'The user information was incorrect'
+            })
+        }
+
+        await user.destroy()
+        res.send(user)
+        } catch (error) {
+            res.status(500).send({
+                error: 'The user information was incorrect'
+                })
+            }
+        },
+
+        // get user by id
+        async show (req, res) {
+            try {
+                const user = await User.findByPk(req.params.userId)
+                res.send(user)
+                } catch (err) {
+                    res.status(500).send({
+                        error: 'The user information was incorrect'
+            })
+        }
+    },
+} 
